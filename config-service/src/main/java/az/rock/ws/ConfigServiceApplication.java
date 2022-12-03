674900742fab10888f40143646cf76a542fc1b1b
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-03 18:38:20 +0400

    Flipo started

--- config-service/src/main/java/az/rock/ws/ConfigServiceApplication.java
+++ config-service/src/main/java/az/rock/ws/ConfigServiceApplication.java
@@ -1,7 +1,17 @@
 package az.rock.ws;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@EnableConfigServer
+@Profile("local")
+@PropertySource("classpath:application.yml")
 public class ConfigServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigServiceApplication.class,args);
     }
 }
--- discovery-service/src/main/java/az/rock/ws/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/az/rock/ws/DiscoveryServiceApplication.java
@@ -1,7 +1,13 @@
 package az.rock.ws;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
