68894c6b36e66749af40e807c9d72e31637b5a65
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-04 13:58:40 +0400

    Flipo started

--- api-gateway/src/main/java/az/rock/ws/ApiGatewayApplication.java
+++ api-gateway/src/main/java/az/rock/ws/ApiGatewayApplication.java
@@ -3,11 +3,10 @@ package az.rock.ws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaClient
-@PropertySource("classpath:application.yml")
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
