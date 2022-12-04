ff804a1e940198bd449204b9a26e5e58ee5a45e3
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-04 22:30:35 +0400

    Flipo started

--- library/JResponse/src/main/java/az/rock/lib/response/JResponseDataTransfer.java
+++ library/JResponse/src/main/java/az/rock/lib/response/JResponseDataTransfer.java
@@ -3,15 +3,8 @@ package az.rock.lib.response;
 import az.rock.lib.JDataTransfer;
 import az.rock.lib.JHeader;
 
-import javax.management.JMRuntimeException;
-import java.util.Objects;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class JResponseDataTransfer<D> extends JDataTransfer<D> {
-    private final static JResponseDataTransfer<?> EMPTY = new JResponseDataTransfer<>(null,Boolean.FALSE);
 
     private final Boolean success;
     private final String message;
@@ -40,65 +33,4 @@ public class JResponseDataTransfer<D> extends JDataTransfer<D> {
         this.message = null;
     }
 
-    @SuppressWarnings("unchecked")
-    public static <D> JResponseDataTransfer<D> ofNullable(D value) {
-        return value == null ? (JResponseDataTransfer<D>) EMPTY : new JResponseDataTransfer<>(value,Boolean.TRUE);
-    }
-
-    public static<D> JResponseDataTransfer<D> EMPTY() {
-        @SuppressWarnings("unchecked")
-        JResponseDataTransfer<D> object = (JResponseDataTransfer<D>) EMPTY;
-        return object;
-    }
-
-    public static <D> JResponseDataTransfer<D> of(D data) {
-        return new JResponseDataTransfer<>(Objects.requireNonNull(data),Boolean.TRUE);
-    }
-
-    public D get() {
-        if (Objects.isNull(super.getData()))
-            throw new JMRuntimeException("No value present");
-        return super.getData();
-    }
-
-    public void ifPresent(Consumer<? super D> action) {
-        if (!Objects.isNull(super.getData())) {
-            action.accept(super.getData());
-        }
-    }
-
-    public void ifPresentOrElse(Consumer<? super D> action, Runnable emptyAction) {
-        if (!Objects.isNull(super.getData())) action.accept(super.getData());
-        else emptyAction.run();
-    }
-
-    public boolean isPresent() {
-        return !Objects.isNull(super.getData());
-    }
-
-    public JResponseDataTransfer<D> filter(Predicate<? super D> predicate) {
-        Objects.requireNonNull(predicate);
-        if (!isPresent()) {
-            return this;
-        } else {
-            return predicate.test(super.getData()) ? this : JResponseDataTransfer.EMPTY();
-        }
-    }
-
-    public <U> JResponseDataTransfer<U> map(Function<? super D, ? extends U> mapper) {
-        Objects.requireNonNull(mapper);
-        if (!isPresent()) return EMPTY();
-        else return JResponseDataTransfer.ofNullable(mapper.apply(super.getData()));
-
-    }
-
-    public JResponseDataTransfer<D> or(Supplier<? extends JResponseDataTransfer<? extends D>> supplier) {
-        Objects.requireNonNull(supplier);
-        if (isPresent()) return this;
-        else {
-            @SuppressWarnings("unchecked")
-            JResponseDataTransfer<D> r = (JResponseDataTransfer<D>) supplier.get();
-            return Objects.requireNonNull(r);
-        }
-    }
 }
