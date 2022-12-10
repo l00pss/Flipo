e5ad626bb677b51ca50766412b0db825c217e00d
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-10 20:51:25 +0400

    Flipo started

--- library/JResponse/src/main/java/az/rock/lib/response/factory/AbstractJSuccessResponseFactory.java
+++ library/JResponse/src/main/java/az/rock/lib/response/factory/AbstractJSuccessResponseFactory.java
@@ -1,4 +1,19 @@
 package az.rock.lib.response.factory;
 
-public interface AbstractJSuccessResponseFactory {
+import az.rock.lib.JHeader;
+import az.rock.lib.response.success.JSuccessDataResponse;
+import az.rock.lib.response.success.JSuccessResponse;
+
+public interface AbstractJSuccessResponseFactory<D> {
+    JSuccessResponse factoryResponse();
+
+    JSuccessResponse factoryResponse(String message);
+
+    JSuccessDataResponse<D> factoryResponse(D data);
+
+    JSuccessDataResponse<D> factoryResponse(JHeader header, D data);
+
+    JSuccessDataResponse<D> factoryResponse(D data, String message);
+
+    JSuccessDataResponse<D> factoryResponse(JHeader header, D data, String message);
 }
