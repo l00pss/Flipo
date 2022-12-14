386f0792d551b24f2f2234f794476b6c8d76e62f
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-14 22:44:23 +0400

    Flipo started

--- auth-service/auth-presentation/src/main/java/az/rock/ws/auth/rest/publics/UserPublicController.java
+++ auth-service/auth-presentation/src/main/java/az/rock/ws/auth/rest/publics/UserPublicController.java
@@ -1,7 +1,9 @@
 package az.rock.ws.auth.rest.publics;
 
 import az.rock.lib.jresponse.request.JRequest;
+import az.rock.lib.jresponse.response.factory.AbstractJSuccessResponseFactory;
 import az.rock.lib.jresponse.response.success.JSuccessDataResponse;
+import az.rock.lib.jresponse.response.success.JSuccessResponse;
 import az.rock.lib.spec.auth.rest.publics.AbstractUserPublicController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/1.0/public/users")
 public class UserPublicController implements AbstractUserPublicController {
+
+    private final AbstractJSuccessResponseFactory<?> responseFactory;
+
+    public UserPublicController(AbstractJSuccessResponseFactory<?> responseFactory) {
+        this.responseFactory = responseFactory;
+    }
+
+    @Override
+    public ResponseEntity<JSuccessResponse> live(JRequest<?> command) {
+        return null;
+    }
 
     @Override
     public ResponseEntity<JSuccessDataResponse<?>> get(JRequest<?> credentials) {
