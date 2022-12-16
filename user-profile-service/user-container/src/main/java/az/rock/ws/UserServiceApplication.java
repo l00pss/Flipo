4f261ceda9c3bf2bc1c96746c64e7fe3fc2274d9
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-16 22:48:31 +0400

    Flipo started

--- user-profile-service/user-container/src/main/java/az/rock/ws/UserServiceApplication.java
+++ user-profile-service/user-container/src/main/java/az/rock/ws/UserServiceApplication.java
@@ -2,9 +2,10 @@ package az.rock.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
