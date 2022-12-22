3e3500db352ef3b07d9b3cfa6b81defc1faf41ae
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-12-22 22:21:38 +0400

    Flipo started

--- api-gateway/src/main/java/az/rock/ws/security/GatewayBeanConfig.java
+++ api-gateway/src/main/java/az/rock/ws/security/GatewayBeanConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayBeanConfig {
     @Bean
-    public RouteLocator userRegistryLocator(RouteLocatorBuilder routeLocatorBuilder, JAuthorizationHeaderFilter
+    public RouteLocator userPrivateLocator(RouteLocatorBuilder routeLocatorBuilder, JAuthorizationHeaderFilter
             authorizationHeaderFilter) {
         return routeLocatorBuilder
                 .routes()
@@ -16,7 +16,23 @@ public class GatewayBeanConfig {
                         .path("/auth-service/1.0/private/auth/**")
                         .filters(f -> f.removeRequestHeader("Cookie")
                                 .rewritePath("/auth-service/(?<segment>.*)", "/$\\{segment}")
-                                .filter(authorizationHeaderFilter.apply(new JAuthorizationHeaderFilter.Config())))
+                                .filter(authorizationHeaderFilter.apply(new JAuthorizationHeaderFilter.Config()))
+                        )
+                        .uri("lb://auth-service")
+                )
+                .build();
+    }
+
+    @Bean
+    public RouteLocator userPublicLocator(RouteLocatorBuilder routeLocatorBuilder, JAuthorizationHeaderFilter
+            authorizationHeaderFilter) {
+        return routeLocatorBuilder
+                .routes()
+                .route(p -> p
+                        .path("/auth-service/1.0/public/auth/**")
+                        .filters(f -> f.removeRequestHeader("Cookie")
+                                .rewritePath("/auth-service/(?<segment>.*)", "/$\\{segment}")
+                        )
                         .uri("lb://auth-service")
                 )
                 .build();
